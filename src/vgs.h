cba7297f0baffa2bdec27f341cca43d51f522de7
Junaid Chaudhry <97283448+BrickSigma@users.noreply.github.com>
2022-11-13 12:32:42 +0300

    Uploaded src files
    Initial upload of template code and Makefile.

---
+++ src/vgs.h
@@ -0,0 +1,90 @@
+#ifndef VGS_H
+#define VGS_H
+
+#ifndef STATUS_ERROR
+#   define STATUS_ERROR (-1)
+#endif
+
+#ifndef STATUS_SUCCESS
+#   define STATUS_SUCCESS (0)
+#endif
+
+/**
+ * @brief Socket descriptor
+ * 
+ */
+typedef unsigned long long Socket;
+
+#ifndef INVALID_SOCKET
+#   define INVALID_SOCKET (Socket)(~0)
+#endif
+
+/**
+ * @brief Initialize the library.
+ * 
+ */
+int InitVGS(void);
+
+/**
+ * @brief Close the library
+ * 
+ */
+int CloseVGS(void);
+
+/**
+ * @brief Create a server socket descriptor and bind it.
+ * 
+ * @param port Port to bind to
+ * @param backlog Number of connections to listen to
+ * 
+ * @return On sucess, the server socket descriptor is returned.
+ */
+Socket StartupServer(int port, int backlog);
+
+/**
+ * @brief Accept any incomming connections from a client
+ * 
+ * @param fd Server socket
+ * 
+ * @return Socket descriptor of the new socket connected 
+ */
+Socket AcceptClient(Socket fd);
+
+/**
+ * @brief Create a client socket descriptor and connect it to a server
+ * 
+ * @param port Port to connect to
+ * @param address IPv4 address of server
+ * 
+ * @return Socket descriptor
+ */
+Socket StartupClient(int port, char *address);
+
+/**
+ * @brief Close the socket
+ * 
+ * @param fd 
+ */
+int CloseSocket(Socket fd);
+
+/**
+ * @brief Send the data in buf.
+ * 
+ * @param fd Socket descriptor
+ * @param buf 
+ * @param len 
+ * @return The number of bytes succesfully sent. Return STATUS_ERROR on failure.
+ */
+int SendData(Socket fd, char *buf, int len);
+
+/**
+ * @brief Read any incomming data and write to buf
+ * 
+ * @param fd Socket descriptor
+ * @param buf 
+ * @param len 
+ * @return The number of bytes succesfully read. Return STATUS_ERROR on failure.
+ */
+int RecvData(Socket fd, char *buf, int len);
+
+#endif // VGS_H
---
+++ src/vgsockets.c
@@ -0,0 +1,112 @@
+#include "vgs.h"
+
+#ifdef _WIN32
+    #include <winsock.h>
+#else
+    // UNIX includes
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+int InitVGS(void)
+{
+#ifdef _WIN32
+    WSADATA wsa;
+    if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
+	{
+		return STATUS_ERROR;
+	}
+
+    return STATUS_SUCCESS;
+
+#else
+    return STATUS_SUCCESS;
+#endif
+}
+
+int CloseVGS(void)
+{
+#ifdef _WIN32
+    WSACleanup();
+    return STATUS_SUCCESS;
+#endif
+}
+
+Socket StartupServer(int port, int backlog)
+{
+    Socket fd = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in server;
+
+	if(fd == INVALID_SOCKET) {
+		return STATUS_ERROR;
+	}
+
+    int opt = 1;
+	if (setsockopt(fd, SOL_SOCKET,
+				SO_REUSEADDR, 
+				(char *)&opt, sizeof(int))) {
+		return STATUS_ERROR;
+	}
+
+    server.sin_family = AF_INET;
+	server.sin_addr.s_addr = INADDR_ANY;
+	server.sin_port = htons((unsigned short)port);
+
+    if (bind(fd ,(struct sockaddr *)&server , sizeof(server)) < 0) {
+		return STATUS_ERROR;
+	}
+
+    if (listen(fd , backlog) < 0) {
+        return STATUS_ERROR;
+    }
+
+    return fd;
+}
+
+Socket AcceptClient(Socket fd)
+{
+    Socket new_socket;
+    struct sockaddr_in client;
+    int c = sizeof(struct sockaddr_in);
+	new_socket = accept(fd , (struct sockaddr *)&client, &c);
+	if (new_socket == INVALID_SOCKET) {
+		return STATUS_ERROR;
+	}
+
+    return new_socket;
+}
+
+Socket StartupClient(int port, char *address)
+{
+    Socket fd = socket(AF_INET , SOCK_STREAM , 0 );
+    struct sockaddr_in server;
+
+    if((signed)fd < 0) {
+		return STATUS_ERROR;
+	}
+
+    server.sin_addr.s_addr = inet_addr(address);
+	server.sin_family = AF_INET;
+	server.sin_port = htons(port);
+
+	//Connect to remote server
+	if (connect(fd , (struct sockaddr *)&server , sizeof(server)) < 0)
+	{
+		return STATUS_ERROR;
+	}
+
+    return fd;
+}
+
+int CloseSocket(Socket fd)
+{
+#ifdef _WIN32
+    closesocket(fd);
+#else
+    printf("Not Implemented!\n");
+#endif
+
+    return STATUS_SUCCESS;
+}
