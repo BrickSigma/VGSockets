fa44ff405e3512041eb2cdf008a0dc6be600375e
Junaid Chaudhry <97283448+BrickSigma@users.noreply.github.com>
2022-11-24 07:13:05 +0300

    Added select based functions

--- src/vgs.h
+++ src/vgs.h
@@ -5,23 +5,41 @@
 extern "C" {
 #endif
 
-#ifndef STATUS_ERROR
-#   define STATUS_ERROR (-1)
-#endif
-
-#ifndef STATUS_SUCCESS
-#   define STATUS_SUCCESS (0)
-#endif
-
 /**
  * @brief Socket descriptor
  * 
  */
 typedef unsigned long long Socket;
 
-#ifndef INVALID_SOCKET
-#   define INVALID_SOCKET (Socket)(~0)
-#endif
+
+// Return codes
+#undef STATUS_ERROR
+#undef STATUS_SUCCESS
+#undef INVALID_SOCKET
+
+#define STATUS_ERROR        (-1)            // Error return code
+#define STATUS_SUCCESS      (0)             // Success return code
+#define INVALID_SOCKET      (Socket)(~0)    // Invalid socket return code
+
+
+// VGSocket related definitions
+#undef FD_SETSIZE
+#define FD_SETSIZE      (64)  // Maximum file descriptor set size
+
+typedef struct _VGSocket {
+    Socket fd;
+    unsigned int fd_count;
+    Socket fd_array[FD_SETSIZE];
+} VGSocket;
+
+typedef struct _Timer {
+    long sec;
+    long msec;
+    long usec;
+} Timer;
+
+
+// ============== CORE MODULES ================
 
 /**
  * @brief Initialize the library.
@@ -92,6 +110,50 @@ int SendData(Socket fd, const void *buf, int len);
  * @return The number of bytes successfully received.
  */
 int RecvData(Socket fd, void *buf, int len);
+
+
+// ============== SELECT/FD_SET MODULES ================
+
+/**
+ * @brief Initialise VGSocket file descriptor set. This should be called after StartupServer
+ * 
+ */
+void InitVGSocket(VGSocket *vgs);
+
+/**
+ * @brief Equivalent to FD_CLR in WinSock.h and sys/select.h
+ * 
+ * @param fd 
+ * @param vgs 
+ */
+void ClearSet(Socket fd, VGSocket *vgs);
+
+/**
+ * @brief Equivalent to FD_ISSET in WinSock.h and sys/select.h
+ * 
+ * @param fd 
+ * @param vgs 
+ * @return int 
+ */
+int IsSet(Socket fd, VGSocket *vgs);
+
+/**
+ * @brief Equivalent to FD_SET in WinSock.h and sys/select.h
+ * 
+ * @param fd 
+ * @param vgs 
+ */
+void AddSet(Socket fd, VGSocket *vgs);
+
+/**
+ * @brief Equivalent to FD_ZERO in WinSock.h and sys/select.h
+ * 
+ * @param vgs 
+ */
+void ZeroSet(VGSocket *vgs);
+
+
+// ============== ERROR MODULES ================
 
 /**
  * @brief Enable internal error messages.
