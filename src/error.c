5c6b6f3f48819c208df2ca9e2d1d373f5933b626
Junaid Chaudhry <97283448+BrickSigma@users.noreply.github.com>
2022-11-25 14:36:16 +0300

    Create error.c
    Error functions placed here.

---
+++ src/error.c
@@ -0,0 +1,34 @@
+#ifdef _WIN32
+    #include <WinSock2.h>
+#endif
+
+#include "vgs.h"
+
+#include <stdio.h>
+#include <errno.h>
+
+// Used to disable/enable error messages.
+int ShowErrorsMsg = 0;
+
+void EnableErrorShow(void)
+{
+    ShowErrorsMsg = 1;
+}
+
+void DisableErrorShow(void)
+{
+    ShowErrorsMsg = 0;
+}
+
+void ShowError(const char *msg)
+{
+    if (ShowErrorsMsg) {
+#ifdef _WIN32
+        fprintf(stderr, "WIN32::%s: %d\n", msg, WSAGetLastError());
+#else
+        static char buff[1024];
+        snprintf(buff, 1024, "UNIX::%s", msg);
+        perror(buff);
+#endif
+    }
+}
