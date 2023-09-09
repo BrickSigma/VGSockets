80439e6a4f6b76f9220a1410ee0a51458705eb10
Junaid Chaudhry <bricksigma176@gmail.com>
2023-09-10 02:25:21 +0300

    Updated timeout example

--- examples/timeout/client.c
+++ examples/timeout/client.c
@@ -20,15 +20,14 @@ int main(void)
 
     // Create a new socket and connect it to the server and port
     printf("Starting up client...\n");
-    VGSocket client;
-    client.fd = StartupClient(8080, "127.0.0.1");
-    if (client.fd == INVALID_SOCKET) {
+    Socket client;
+    client = StartupClient(8080, "127.0.0.1");
+    if (client == INVALID_SOCKET) {
         return -1;
     }
-    InitVGSocket(&client);  // Initialize the VGSocket
 
     printf("Sending data...\n");
-    int valsent = SendData(client.fd, "Hello server!", 14);
+    int valsent = SendData(client, "Hello server!", 14);
     if (valsent < 0) {
         return -1;
     }
@@ -37,20 +36,20 @@ int main(void)
     // Set the timeout duration for the TimedRecv function using
     // the Timer struct
     char buff[256];
-    Timer t = {.sec=3, .msec=0, .usec=0};
     printf("Waiting for server...\n");
-    int valread = TimedRecv(client, client.fd, buff, 256, &t);
-    if (valread == 0) {
-        printf("Timed out!\n");
-    } else if (valread < 0) {
-        return -1;
+    int valrecv = TimedRecv(client, buff, 256, 3000);  // Recv set to timeout after 3 seconds
+    if (valrecv < 0) {
+        perror("Error in receiving data");
+    } else if (valrecv == 0) {
+        printf("Timed out.\n");
     } else {
-        printf("Data received: %s. \nNumber of bytes received: %d\n", buff, valread);
+        printf("Date received: %s\n", buff);
     }
+    
 
     // Cleanup
     printf("Closing client and VGS...\n");
-    CloseSocket(client.fd);
+    CloseSocket(client);
     CloseVGS();
     return 0;
 }
