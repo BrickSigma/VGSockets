162d48ffd1223f26d23795bf6277acae1b07c8e4
Junaid Chaudhry <bricksigma176@gmail.com>
2023-09-13 22:57:43 +0300

    Added timeout for UDP RecvFrom and fixed poll bug
    Poll was not functioning correctly due to differences in the nature of `Pollfd` defined in `vgs.h` and `struct pollfd` in the UNIX header `poll.h` and typecasting.

--- src/poll.c
+++ src/poll.c
@@ -7,22 +7,52 @@
 
 #include "vgs.h"
 
+#include <stdlib.h>
+
 
 int Poll(Pollfd *fds, unsigned long nfds, int timeout) {
+    struct pollfd *pfds = calloc(nfds, sizeof(struct pollfd));
+    for (unsigned long i = 0; i < nfds; i++) {
+        pfds[i].fd = fds[i].fd;
+        pfds[i].events = fds[i].events;
+    }
+
+    int retvalue;
 #if defined _WIN32 || defined __CYGWIN__
-    return WSAPoll((struct pollfd *)fds, (unsigned long)nfds, timeout);
+    retvalue =  WSAPoll(pfds, (unsigned long)nfds, timeout);
 #else
-    return poll((struct pollfd *)fds, (nfds_t)nfds, timeout);
+    retvalue = poll(pfds, (nfds_t)nfds, timeout);
 #endif
+
+    free(pfds);
+
+    return retvalue;
 }
 
 int TimedRecv(Socket fd, void *buf, int len, int timeout)
 {
-    Pollfd pfd = {.fd = fd, .events=POLLIN};
+    struct pollfd pfd = {.fd = fd, .events = POLLIN};
     int valrecv = 0;
-    int pollval = Poll(&pfd, 1, timeout);
+    int pollval = poll(&pfd, 1, timeout);
     if (pollval > 0 && (pfd.revents & POLLIN)) {
         valrecv = recv(fd, buf, len, 0);
+        if (valrecv < 0) {
+            ShowError("ERROR RECEIVING");
+        }
+    } else if (pollval < 0) {
+        ShowError("ERROR POLLING FILE DESCRIPTOR");
+    }
+
+    return valrecv;
+}
+
+int TimedRecvFrom(Socket fd, void *buf, int len, Sockaddr *address, int timeout)
+{
+    struct pollfd pfd = {.fd = fd, .events = POLLIN};
+    int valrecv = 0;
+    int pollval = poll(&pfd, 1, timeout);
+    if (pollval > 0 && (pfd.revents & POLLIN)) {
+        valrecv = RecvFrom(fd, buf, len, address);
         if (valrecv < 0) {
             ShowError("ERROR RECEIVING");
         }
